9868383df86204f96906c36eca43bcd73fc145c8
aldo <geroelectronics@hotmail.com>
2023-04-02 22:14:32 -0500

    proy.251

--- src/app.js
+++ src/app.js
@@ -1,106 +1,20 @@
 const express = require('express')
-const { userRouter } = require('./routes/users.router')
+//const { userRouter } = require('./routes/users.router')
 const productsRouter = require('./routes/products')
 const cartsRouter = require('./routes/carts')
 
 const app = express()
 const PORT = 4000
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+//app.use(express.json())
+//app.use(express.urlencoded({extended:true}))
 
-app.use('/api/products',useRouter)
-app.use('/api/carts',useRouter)
+app.use('/api/products',productsRouter)
+app.use('/api/carts',cartsRouter)
 
 
 app.listen(PORT ,(err)=> {
-    if(err)return console.log("Error al iniciar el sevidor")
+  if(err)return console.log("Error al iniciar el sevidor")
     console.log("servidor iniciado en el purto${PORT}")
 })
 
-
-
-
-
-const readDataFromFile = (filePath) => {
-  const rawData = fs.readFileSync(filePath);
-  const data = JSON.parse(rawData);
-  return data;
-};
-
-const writeDataToFile = (filePath, data) => {
-  const rawData = JSON.stringify(data, null, 2);
-  fs.writeFileSync(filePath, rawData);
-};
-
-
-const generateId = () => {
-  return Math.floor(Math.random() * 1000000);
-};
-
-
-const productsRouter = express.Router();
-
-productsRouter.get('/', (req, res) => {
-  const products = readDataFromFile(PRODUCTS_DB_PATH);
-  const limit = req.query.limit || products.length;
-  const result = products.slice(0, limit);
-  res.json(result);
-});
-
-productsRouter.get('/:pid', (req, res) => {
-  const products = readDataFromFile(PRODUCTS_DB_PATH);
-  const product = products.find((p) => p.id == req.params.pid);
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).send('Product not found');
-  }
-});
-
-productsRouter.post('/', (req, res) => {
-  const products = readDataFromFile(PRODUCTS_DB_PATH);
-  const newProduct = {
-    id: generateId(),
-    title: req.body.title,
-    description: req.body.description,
-    code: req.body.code,
-    price: req.body.price,
-    status: req.body.status || true,
-    stock: req.body.stock,
-    category: req.body.category,
-    thumbnails: req.body.thumbnails || [],
-  };
-  products.push(newProduct);
-  writeDataToFile(PRODUCTS_DB_PATH, products);
-  res.status(201).json(newProduct);
-});
-
-productsRouter.put('/:pid', (req, res) => {
-  const products = readDataFromFile(PRODUCTS_DB_PATH);
-  const index = products.findIndex((p) => p.id == req.params.pid);
-  if (index !== -1) {
-    const updatedProduct = {
-      ...products[index],
-      title: req.body.title,
-      description: req.body.description,
-      code: req.body.code,
-      price: req.body.price,
-      status: req.body.status,
-      stock: req.body.stock,
-      category: req.body.category,
-      thumbnails: req.body.thumbnails || products[index].thumbnails,
-    };
-    products[index] = updatedProduct;
-    writeDataToFile(PRODUCTS_DB_PATH, products);
-    res.json(updatedProduct);
-  } else {
-    res.status(404).send('Product not found');
-  }
-});
-
-productsRouter.delete('/:pid', (req, res) => {
-  const products = readDataFromFile(PRODUCTS_DB_PATH);
-  const index = products.findIndex((p) => p.id == req.params.pid);
-
-});
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,20 +1,56 @@
-const fs = require('fs');
-const path = require('path');
+const { Router } = require('express')
 
-const PRODUCTS_FILE = path.join(__dirname, '..', 'data', 'productos.json');
+const userRouter = Router()
 
-function readProductsFile() {
-  const data = fs.readFileSync(PRODUCTS_FILE);
-  return JSON.parse(data);
-}
 
-function writeProductsFile(data) {
-  const jsonData = JSON.stringify(data, null, 2);
-  fs.writeFileSync(PRODUCTS_FILE, jsonData);
-}
+let users = []
 
-function generateProductId() {
-  const products = readProductsFile();
-  const ids = products.map((p) => p.id);
-  const newId = ids.length > 0 ? Math.max(...ids) + 1 : 1;
+// GET http://localhost:xxxx /api/products  /     (listar productos de base)
+userRouter.get('/', (req, res)=>{
+    
+    res.send('get de usuarios')
+})
+
+// GET:/pid  http://localhost:xxxx /api/products  //(traer producto con id)
+
+app.get('/:userId', (req, res)=>{  
+  const { userId } = req.params
+
+  const usuario = arrayUsuarios.find((usuario) => usuario.id === userId)
+
+  if (!usuario) return res.status(400).send('no se encontro el usuario') 
+
+  res.status(200).send(usuario)
+})
+
+
+// POST http://localhost:xxxx /api/products  /  (post,nuevo producto)
+userRouter.post('/', (req, res)=>{
+    const {title,description,code,price,status,stock,category ,thumbnails } = req.body
+    users.push({ id:Date.now(), title, description,code, price ,status ,stock ,category ,thumbnails })
+    return res.json({
+        status: 'success',
+        message: 'usuario agregado correctamente',
+        users
+    })
+})
+
+// PUT http://localhost:xxxx /api/products / PUT/:pid (tomar producto desde body)
+
+
+
+
+
+// DELETE /:pid http://localhost:xxxx /api/products  /
+     app.delete('/api/products/:pid', async (req, res) => {
+    
+  const { pid } = req.params
+
+  // validar el id
+  if (!pid) {
+      return res.status(400).send('manda el id')
+  }})
+
+module.exports = { 
+    userRouter
 }
